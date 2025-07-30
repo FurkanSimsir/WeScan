dda9fba6504c844fddfcc8ef4a516ba40cd17018
Furkan Simsir <furkan@sir.studio>
2025-07-30 11:09:56 +0200

    Make ImageScannerControllerRepresentable public

--- Sources/WeScan/Crop/ImageScannerControllerRepresentable.swift
+++ Sources/WeScan/Crop/ImageScannerControllerRepresentable.swift
@@ -8,39 +8,45 @@
 import SwiftUI
 import UIKit
 
-struct ImageScannerControllerRepresentable: UIViewControllerRepresentable {
+public struct ImageScannerControllerRepresentable: UIViewControllerRepresentable {
   var image: UIImage
   var onImageScanned: (UIImage) -> Void
   var onCancel: () -> Void
 
-  func makeUIViewController(context: Context) -> WeScan.ImageScannerController {
+  public init(image: UIImage, onImageScanned: @escaping (UIImage) -> Void, onCancel: @escaping () -> Void) {
+    self.image = image
+    self.onImageScanned = onImageScanned
+    self.onCancel = onCancel
+  }
+
+  public func makeUIViewController(context: Context) -> WeScan.ImageScannerController {
     WeScan.ImageScannerController(image: image, delegate: context.coordinator)
   }
 
-  func updateUIViewController(_ uiViewController: WeScan.ImageScannerController, context: Context) {}
+  public func updateUIViewController(_ uiViewController: WeScan.ImageScannerController, context: Context) {}
 
-  func makeCoordinator() -> Coordinator {
+  public func makeCoordinator() -> Coordinator {
     Coordinator(self)
   }
 
-  class Coordinator: NSObject, ImageScannerControllerDelegate {
+  public class Coordinator: NSObject, ImageScannerControllerDelegate {
     var parent: ImageScannerControllerRepresentable
 
     init(_ parent: ImageScannerControllerRepresentable) {
       self.parent = parent
     }
 
-    func imageScannerController(_ scanner: ImageScannerController, didFailWithError error: Error) {
+    public func imageScannerController(_ scanner: ImageScannerController, didFailWithError error: Error) {
       print("Error occurred during scanning: \(error)")
       parent.onCancel()
     }
 
-    func imageScannerController(_ scanner: ImageScannerController, didFinishScanningWithResults results: ImageScannerResults) {
+    public func imageScannerController(_ scanner: ImageScannerController, didFinishScanningWithResults results: ImageScannerResults) {
       parent.onImageScanned(results.userSelectedScan.image)
       scanner.dismiss(animated: true)
     }
 
-    func imageScannerControllerDidCancel(_ scanner: ImageScannerController) {
+    public func imageScannerControllerDidCancel(_ scanner: ImageScannerController) {
       parent.onCancel()
       scanner.dismiss(animated: true)
     }
